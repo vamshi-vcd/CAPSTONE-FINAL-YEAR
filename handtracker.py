2ef50d0d9a2be02ad381e66094db0a3fd62f6671
vamshi-vcd <113637912+vamshi-vcd@users.noreply.github.com>
2024-05-07 16:34:31 +0530

    Update handtracker.py

--- handtracker.py
+++ handtracker.py
@@ -6,6 +6,10 @@
 
 import cv2
 import mediapipe as mp
+import os
+# Set a dummy value for the 'DISPLAY' environment variable
+os.environ['DISPLAY'] = ':0.0'
+
 import pyautogui
 
 cap = cv2.VideoCapture(0)
